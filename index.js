e120dec50af6c899472be801ba9f1c7ee1d7fe19
Scott Hoisington <hoyzington@gmail.com>
2021-09-07 19:48:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+	let min = array[0];
+	let idx;
+	for (let i = 0; i < array.length; i++) {
+		if (array[i] < min) {
+			min = array[i];
+			idx = i;
+		}
+	}
+	array.splice(idx, 1);
+	return min;
 }
 
 function selectionSort(array){
-
+	const result = [];
+	while (array.length != 0) {
+		result.push(findMinAndRemove(array));
+	}
+	return result;
 }
